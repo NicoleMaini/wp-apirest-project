e40ae320a17c99b18d8676fd31c5463cc70156dd
Nico_le <156796855+NicoleMaini@users.noreply.github.com>
2024-04-30 17:01:31 +0200

    add pagination

--- src/componenets/HomeWp.jsx
+++ src/componenets/HomeWp.jsx
@@ -1,20 +1,18 @@
 import { useEffect, useState } from "react";
-import { Container, Pagination, Row } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import { apiUrl } from "../constants";
 import PostWp from "./PostsWp";
-import PaginationWp from "./PaginationWp";
 
 function HomeWp() {
   //settiamo la pagina iniziale
   const [posts, setPosts] = useState([]);
-  // const [lastPage, setLastPage] = useState(null);
+  const [lastPage, setLastPage] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(6);
 
   useEffect(() => {
-    fetch(apiUrl + `/posts?page=${currentPage}&limit=${postsPerPage}&_embed=1`)
+    fetch(apiUrl + `/posts?page=${currentPage}`)
       .then(resp => {
-        // setLastPage(parseInt(resp.headers.get("X-WP-TotalPages")));
+        setLastPage(parseInt(resp.headers.get("X-WP-TotalPages")));
         return resp.json();
       })
       .then(data => {
@@ -22,37 +20,52 @@ function HomeWp() {
         setPosts(data);
       })
       .catch(err => console.log("C'è un errore:", err));
-  }, [posts, currentPage, postsPerPage]);
+  }, [posts]);
 
-  // const totalPages = Math.ceil(posts.length / postsPerPage);
-  const startIndex = (currentPage - 1) * postsPerPage;
-  const endIndex = startIndex + postsPerPage;
-  const currentPosts = posts.slice(startIndex, endIndex);
-
-  console.log(currentPosts);
-
-  const handleclickNext = () => {
-    return setCurrentPage(currentPage + 1);
+  const changePage = page => {
+    setCurrentPage(page);
   };
-  const handleclickBack = () => {
-    return setCurrentPage(currentPage - 1);
-  };
+
+  function generatePaginationArray() {
+    let paginationArr = [];
+    for (let index = 1; index <= lastPage; index++) {
+      paginationArr.push({
+        n: index,
+        active: currentPage === index,
+      });
+    }
+    return paginationArr;
+  }
 
   return (
     <Container>
       <Row>
-        {currentPosts.map(post => (
+        {posts.map(post => (
           <PostWp key={post.id} post={post} />
         ))}
       </Row>
 
-      <Pagination className="my-5 d-flex justify-content-center">
-        <Pagination.Prev onClick={handleclickBack} />
-        <Pagination.Item>{currentPage - 1}</Pagination.Item>
-        <Pagination.Item active>{currentPage}</Pagination.Item>
-        <Pagination.Item>{currentPage + 1}</Pagination.Item>
-        <Pagination.Next onClick={handleclickNext} />
-      </Pagination>
+      <ul className="pagination my-5 d-flex justify-content-center">
+        <li className={`page-item ${currentPage === 1 && "disabled"}`}>
+          <span className="page-link" onClick={() => currentPage !== 1 && changePage(currentPage - 1)}>
+            Previous
+          </span>
+        </li>
+
+        {generatePaginationArray().map(page => (
+          <li key={page.n} className={`page-item ${page.active && "active"}`}>
+            <span className="page-link" onClick={() => changePage(page.n)}>
+              {page.n}
+            </span>
+          </li>
+        ))}
+
+        <li className={`page-item ${currentPage === "lastPage" && "disabled"}`}>
+          <span className="page-link" onClick={() => currentPage !== lastPage && changePage(currentPage + 1)}>
+            Next
+          </span>
+        </li>
+      </ul>
     </Container>
   );
 }
